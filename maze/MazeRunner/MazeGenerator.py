379ae4dbd8c974430941c4639c65124e226b73f9
aakash018 <aakashkhanal015@gmail.com>
2021-06-16 15:35:49 +0545

    collision works with button holding bug

--- maze/MazeRunner/MazeGenerator.py
+++ maze/MazeRunner/MazeGenerator.py
@@ -1,55 +1,60 @@
-from random import shuffle, randrange
+from random import randrange, shuffle
+
 
 class MazeGenerator:
-	def __init__(self, row=None, column=None):
-		self.row = row or 12
-		self.column = column or 12
+    def __init__(self, row=None, column=None):
+        self.row = row or 12
+        self.column = column or 12
 
-	def write(self, filename):
-		f = open(filename, 'w')
-		f.write(self.make_maze(self.row, self.column))
-		f.close()
+    def write(self, filename):
+        f = open(filename, 'w')
+        f.write(self.make_maze(self.row, self.column))
+        f.close()
 
-	def make_maze(self, w, h):
-	    grid = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
-	    col = [["X "] * w + ['X'] for _ in range(h)] + [[]]
-	    row = [["XX"] * w + ['X'] for _ in range(h + 1)]
+    def make_maze(self, w, h):
+        grid = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
+        col = [["X "] * w + ['X'] for _ in range(h)] + [[]]
+        row = [["XX"] * w + ['X'] for _ in range(h + 1)]
 
-	    # debug helper
+        # debug helper
 
-	    # def debugPrint():
-	    #     print("-"*16)
-	    #     s = ""
-	    #     for (a, b) in zip(row, col):
-	    #         s += ''.join(a + ['\n'] + b + ['\n'])
-	    #     print(s )
+        # def debugPrint():
+        #     print("-"*16)
+        #     s = ""
+        #     for (a, b) in zip(row, col):
+        #         s += ''.join(a + ['\n'] + b + ['\n'])
+        #     print(s )
 
-	    #     for r in grid:
-	    #         print(r) 
+        #     for r in grid:
+        #         print(r)
 
+        def walk(x, y):
 
-	    def walk(x, y):
+            # debug helper
 
-	    	#debug helper
+            # debugPrint()
 
-	        # debugPrint()
+            grid[y][x] = 1  # mark random cell as visited
 
-	        grid[y][x] = 1 # mark random cell as visited
+            # getting possible neighbours
+            d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
+            shuffle(d)  # shuffling possible neighbours
+            for (xx, yy) in d:
+                if grid[yy][xx]:
+                    continue  # ignore if visited cell or a wall
+                if xx == x:
+                    row[max(y, yy)][x] = "X "  # randomly
+                if yy == y:
+                    col[y][max(x, xx)] = "  "  # open walls
 
-	        d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)] # getting possible neighbours
-	        shuffle(d) # shuffling possible neighbours
-	        for (xx, yy) in d:
-	            if grid[yy][xx]: continue # ignore if visited cell or a wall
-	            if xx == x: row[max(y, yy)][x] = "X " # randomly
-	            if yy == y: col[y][max(x, xx)] = "  " # open walls
+                walk(xx, yy)
 
-	            walk(xx, yy)
+        walk(randrange(w), randrange(h))
 
-	    walk(randrange(w), randrange(h))
+        s = ""
+        for (a, b) in zip(row, col):
+            s += ''.join(a + ['\n'] + b + ['\n'])
+        return s
 
-	    s = ""
-	    for (a, b) in zip(row, col):
-	        s += ''.join(a + ['\n'] + b + ['\n'])
-	    return s
 
 MazeGenerator()
--- maze/main.py
+++ maze/main.py
@@ -1,4 +1,3 @@
-import turtle
 from random import randrange, shuffle
 
 from .MazeRunner.GameObject import GameObject
@@ -13,13 +12,18 @@ generator.write('Block.txt')
 WINWIDTH = 625
 WINHEIGHT = 625
 TITLE = "MazeRunner"
+BGCOLOR = "#0E0F1E"
+BUTTONBG = "#1F3049"
 
 GRIDWIDTH = 25
 GRIDHEIGHT = 25
 GRIDXORIG = -302.5
 GRIDYORIG = 302.5
 
-SCALE = 20
+SCALE = 25
+
+# * Wall Cordinate
+wallsCordsInScreen = []
 
 # Screen stufss
 # screen = turtle.Screen()
@@ -27,8 +31,6 @@ SCALE = 20
 # screen.setup(WINWIDTH, WINHEIGHT)
 
 # Game Objects
-wall = GameObject("square", "black", 1)
-space = GameObject("square", "white", 1)
 
 # Read generated grid from MazeGenerator
 f = open('Block.txt', 'r')
@@ -36,15 +38,22 @@ grid = f.readlines()
 
 
 def drawMaze():
+    wall = GameObject("square", BUTTONBG, 1)
+    space = GameObject("square", BGCOLOR, 1)
     for i in range(GRIDHEIGHT):
         for j in range(GRIDWIDTH):
             wall.goto(GRIDXORIG + (j*SCALE), GRIDYORIG - (i*SCALE))
             space.goto(GRIDXORIG + (j*SCALE), GRIDYORIG - (i*SCALE))
             if(grid[i][j] == "X"):
                 wall.stamp()
+                wallsCordsInScreen.append((wall.xcor(), wall.ycor()))
             if(grid[i][j] == " "):
                 space.stamp()
     # turtle.done()
+
+
+def getWallsCords():
+    return wallsCordsInScreen
 
 
 if __name__ == '__main__':
