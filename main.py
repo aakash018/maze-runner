fff9e2ea57bf59cb0f66ca0ab83811f032b156a1
Bigyan Dahal <iambigyandahal@gmail.com>
2021-06-17 14:56:24 +0545

    mulitplayer functionality

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ BUTTONBG = "#1F3049"
 WIDTH = 900
 HEIGHT = 600
 
+gameMode = ""
+
 # * SCREN INIT
 screen = Screen()
 screen.setup(WIDTH, HEIGHT)
 screen.bgcolor(BGCOLOR)
 screen.bgpic("./assets/images/bg.png")
-
 
 # * WELCOME SCREEN START BUTTON
 FONT_SIZE = 36
@@ -25,8 +26,10 @@ FONT = ('Courier', FONT_SIZE, 'bold')
 
 makeButton.button(FONT_SIZE, "START", 0, 0, 5, 20)
 
+makeButton.button(FONT_SIZE - 20, "Multiplayer", 0, -100)
+
 # * WELCOM SCREEN ABOUT US BUTTON
-makeButton.button(FONT_SIZE - 20, "About", 0, -100)
+makeButton.button(FONT_SIZE - 20, "About", 0, -200)
 
 
 # * Character Moment Functions
@@ -112,15 +115,46 @@ def startGame():
     screen.onkeypress(lambda: moveRight(character), "d")
     screen.listen()
 
+def multi():
+    screen.clear()
+    screen.bgcolor(BGCOLOR)
+    mazeMaker.drawMaze()
+
+    character1 = Turtle()
+    character1.shape("square")
+    character1.color("yellow")
+    character1.penup()
+
+    character2 = Turtle()
+    character2.shape("square")
+    character2.color("yellow")
+    character2.penup()
+    # character.goto(-277.5, 302.5 - 25)
+    character1.goto(mazeMaker.getStartCords())
+    character2.goto(mazeMaker.getStartCords())
+
+    screen.onkeypress(lambda: moveUp(character1), "w")
+    screen.onkeypress(lambda: moveDown(character1), "s")
+    screen.onkeypress(lambda: moveLeft(character1), "a")
+    screen.onkeypress(lambda: moveRight(character1), "d")
+
+    screen.onkeypress(lambda: moveUp(character2), "Up")
+    screen.onkeypress(lambda: moveDown(character2), "Down")
+    screen.onkeypress(lambda: moveLeft(character2), "Left")
+    screen.onkeypress(lambda: moveRight(character2), "Right")
+    screen.listen()
 
 def onClick(clickedX, clickedY):
+    global gameMode
+
     FONT = ('Courier', 20, 'bold')
     COLOR = "white"
 
     # ? IF ABOUT ME IS PRESSES
     if(clickedX > -100 and clickedX < 101 and clickedY > -116 and clickedY < -85):
-        screen.clear()
-        screen.bgcolor(BGCOLOR)
+        # screen.clear()
+        # screen.bgcolor(BGCOLOR)
+
         # name1 = Turtle()
         # name1.hideturtle()
         # name1.color(COLOR)
@@ -130,19 +164,29 @@ def onClick(clickedX, clickedY):
         # name2.hideturtle()
         # name2.color(COLOR)
         # name2.write("Manish", align='center', font=FONT)
+        gameMode = "multi"
+        multi()
 
     print(clickedX, clickedY)
     # ? IF START BUTTON IS PRESSES
     if(clickedX > -200 and clickedX < 200 and clickedY > -50 and clickedY < 50):
+        gameMode = "single"
         startGame()
 
 
 def onRestartClick(clickedX, clickedY):
+    print("clickedout")
     if(clickedX > -200 and clickedX < 205 and clickedY > -166 and clickedY < -134):
-        print("clicked")
-        mazeMaker.resetWallsCords()
-        mazeMaker.resetSpacesCords()
-        startGame()
+        print("clickediin")
+        print(gameMode)
+        if(gameMode == "single"): 
+            mazeMaker.resetWallsCords()
+            mazeMaker.resetSpacesCords()
+            startGame()
+        if(gameMode == "multi"):
+            mazeMaker.resetWallsCords()
+            mazeMaker.resetSpacesCords()
+            multi()
 
 
 onscreenclick(onClick)
